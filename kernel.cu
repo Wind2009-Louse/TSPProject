abb12a5bb603f003642149cfc15aed075aa79f84
Wind2009-Louse <wind.jackey@qq.com>
2019-06-05 23:54:30 +0800

    Add gpu-solver(unfinished)
    TODO:
    * kernel part
    * find the best sequence among the results
    * early-stop judgement

--- kernel.cu
+++ kernel.cu
@@ -201,10 +201,89 @@ thrust::host_vector<int> serial_TSP(thrust::host_vector<Vertex> maps, int max_tr
 	return sequence;
 }
 
+// kernal part of TSP gpu-solver
+// input:
+//   map: a one-dimension vector on behalf of a 2d matrix
+//        use map[y * map_width + x] to fetch it.
+//   map_width: the width of the map
+//              the map is a square, so width = height
+//   max_trial: max trial times in this function
+//   max_retry: max retry count for each trial
+//              if reach the max_retry, it will stop in advanced
+//   parallel_count: count of computation in a row
+// output:
+//   is_refused: a 1d vector recording each thread's return status.
+//               if it's true, the thread stop in advanced.
+// input/output: 
+//   sequences_list: a 1d vector on behalf of a 2d matrix
+//                   use sequences_list[tid * map_width] to fetch it (length map_width)
+__global__ void gpu_TSP_kernel(
+	float* map, const int map_width,
+	int max_trial, int max_retry, int parallel_count,
+	bool* is_refused, int* sequences_list
+) {
+	int tid = blockDim.x * blockIdx.x + threadIdx.x;
+	// TODO
+}
+
+// host part of TSP gpu-solver
+// input:
+//   maps: a vector of vertex ordered by its id
+//   max_trial: max trial times
+//   max_retry: max retry count for each trial
+//              if reach the max_retry, it will stop in advanced
+//   parallel_count: count of computation in a row
+//   trial_per_loop: trial times for per loop
+// output: a vector with the (maybe) best way.
+thrust::host_vector<int> gpu_TSP_host(thrust::host_vector<Vertex> maps, int max_trial = 5000, int max_retry = 1000, int parallel_count = 128, int trial_per_loop = 100) {
+	// make sequence
+	thrust::host_vector<int> sequence = make_random_sequence(maps);
+
+	// transform maps into kernel form
+	auto map_size = maps.size();
+	thrust::host_vector<float> host_distance_map;
+	for (int i = 0; i < map_size; ++i) {
+		thrust::host_vector<float> _v = maps[i].distances;
+		host_distance_map.insert(host_distance_map.end(), _v.begin(), _v.end());
+	}
+	thrust::device_vector<float> device_distance_map = host_distance_map;
+
+	// run loop
+	while (max_trial > 0) {
+		// transform sequence into kernel form(for each thread)
+		thrust::host_vector<int> host_sequence;
+		for (int i = 0; i < parallel_count; ++i) {
+			host_sequence.insert(host_sequence.end(), sequence.begin(), sequence.end());
+		}
+		thrust::device_vector<int> device_sequence = host_sequence;
+
+		// initialize refuse vector
+		thrust::device_vector<bool> is_refused(parallel_count, false);
+
+		// initialize trial times for this times
+		int trial_times = max_trial > trial_per_loop ? trial_per_loop : max_trial;
+		max_trial -= trial_times;
+
+		// run kernel
+		gpu_TSP_kernel (
+			thrust::raw_pointer_cast(&device_distance_map[0]), map_size,
+			trial_times, max_retry, parallel_count,
+			thrust::raw_pointer_cast(&is_refused[0]), thrust::raw_pointer_cast(&device_sequence[0]));
+
+		// find the best sequence
+		// TODO
+
+		// judge whether all-stop
+		// TODO
+	}
+	return sequence;
+}
+
 // main function
 int main() {
 	thrust::host_vector<Vertex> maps = read_xml_map("samples/xml/att532.xml");
-	thrust::host_vector<int> way_result = serial_TSP(maps, 100000, 10000);
+	//thrust::host_vector<int> way_result = serial_TSP(maps, 100000, 10000);
+	gpu_TSP_host(maps);
 
 	return 0;
 }
